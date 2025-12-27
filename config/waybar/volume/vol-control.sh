f643a707f51653794dc694f1428474c7255c6021
Hugo Vidal <hugovima09@gmail.com>
2025-12-27 16:59:57 +0100

    arreglo vol

--- config/waybar/volume/vol-control.sh
+++ config/waybar/volume/vol-control.sh
@@ -6,23 +6,28 @@ show(){
 	fi
 }
 
-interaction="/home/hvidal/.config/waybar/volume/interaction.txt"
+INTERACTION="/home/hvidal/.config/waybar/volume/interaction.txt"
+LOCKFILE="/tmp/vol.lock"
 
-case $1 in
-	"up")
-		wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-		date +%s > $interaction
-		show
-	;;
-	"down")
-		wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-
-		date +%s > $interaction
-		show
-	;;
-	"mute")
-		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-		date +%s > $interaction
-		show
-	;;
-esac
+if ! [ -e $LOCKFILE ];then  # Esto es por si se ejecuta dos veces. Con este if solo se ejecuta una vez por pulsacion.
+	touch $LOCKFILE
+	case $1 in
+		"up")
+			wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+			date +%s > $INTERACTION
+			show
+		;;
+		"down")
+			wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%-
+			date +%s > $INTERACTION
+			show
+		;;
+		"mute")
+			wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+			date +%s > $INTERACTION
+			show
+		;;
+	esac
+	rm $LOCKFILE
+fi
 
