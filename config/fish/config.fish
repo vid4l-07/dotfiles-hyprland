0e52560ef3665469456d6fd1eeb6294b3fc94e0b
Hugo Vidal <hugovima09@gmail.com>
2026-03-23 19:16:11 +0100

    fis vim movement

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+fish_vi_key_bindings # vim movement
+
+function fish_mode_prompt
+end
 
 # Aliases
 alias ls='lsd'
